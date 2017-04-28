6bcbed5181479d120732a4e237cd93fecfd9860d
Joshua Williams <jjwill10@ncsu.edu>
2017-04-28 16:07:03 -0400

    Articles resource added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  
+
+  resources :articles
+
+  ##TELLS RAILS TO MAP THE ROOT OF THE APPLICATION TO THIS CONTROLLER'S METHOD.
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
