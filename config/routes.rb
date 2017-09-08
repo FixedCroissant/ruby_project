aac52b8b571395f63721f1069601609de121f5d7
Joshua Williams <jjwill10@ncsu.edu>
2017-09-08 15:40:53 -0400

    Updates
    1.) Added the rails_admin gem, minor changes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  #mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   get 'welcome/index'
-  
+
 
   resources :articles  do
   	resources  :comments
