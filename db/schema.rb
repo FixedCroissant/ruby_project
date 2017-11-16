9a7aaee2d14245877d35dc2b24b986516206a1cb
Joshua Williams <jjwill10@ncsu.edu>
2017-11-16 14:10:18 -0500

    Running tests, removed old migration
    1.) Trying the basics of running tests for the first time within Ruby.
    2.) Removed old migration for rolify
    3.) Removed the old authority initalizer.
    4.) Added dummy field for user e-mail address when running test.
    5.) Save old version of the users table migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113192200) do
-
-  create_table "_users_old_20171113", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.boolean  "admin"
-  end
+ActiveRecord::Schema.define(version: 20171114183559) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
